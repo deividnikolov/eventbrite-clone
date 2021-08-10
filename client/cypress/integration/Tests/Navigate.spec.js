4af680e1da7daaa62574ddc86b57e644b1a6b1f1
deividnikolov <davidnikolov990@gmail.com>
2021-08-11 01:37:58 +0300

    New folders, tests added

---
+++ client/cypress/integration/Tests/Navigate.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+it('should navigate to base page', () => {
+  cy.visit(Cypress.env('baseUrl'));
+  cy.wait(500);
+  cy.get('.brand-logo').should('be.visible');
+});
+it('should navigate to register page', () => {
+  cy.visit(Cypress.env('registerUrl'));
+  cy.get('.btn').should('be.visible');
+});
+it('should navigate to login page', () => {
+  cy.visit(Cypress.env('loginUrl'));
+  cy.wait(500);
+  cy.get('[type="button"]').should('be.visible');
+});
---
+++ client/cypress/integration/Tests/Viewports.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+const phoneModels = [
+  'iphone-4',
+  'iphone-5',
+  'iphone-6',
+  'iphone-7',
+  'iphone-8',
+  'iphone-x',
+  'iphone-xr',
+  'iphone-se2',
+  'samsung-note9',
+  'samsung-s10',
+];
+describe('Login Page Logo On Mobile', () => {
+  phoneModels.forEach((model) => {
+    it(`Should display logo on ${model} screen`, () => {
+      cy.viewport(model);
+      cy.visit(Cypress.env('baseUrl'));
+      cy.get('.brand-logo').should('be.visible');
+    });
+  });
+
+  const macVersions = ['macbook-11', 'macbook-13', 'macbook-15', 'macbook-16'];
+
+  describe('Login Page Logo On Desktop', () => {
+    macVersions.forEach((version) => {
+      it(`Should display login logo on  ${version}`, () => {
+        cy.viewport(version);
+        cy.get('.brand-logo').should('be.visible');
+      });
+    });
+  });
+});
