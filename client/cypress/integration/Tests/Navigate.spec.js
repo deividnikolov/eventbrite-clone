27e56b218a3a189e2c3120c09e49cbf00adac6c4
deividnikolov <davidnikolov990@gmail.com>
2021-08-11 05:11:16 +0300

    formatting

--- client/cypress/integration/Tests/Navigate.spec.js
+++ client/cypress/integration/Tests/Navigate.spec.js
@@ -1,17 +1,23 @@
 /// <reference types="cypress" />
 it('should navigate to base page', () => {
-  cy.visit(Cypress.env('baseUrl'));
+  cy.visit(Cypress
+    .env('baseUrl'));
   cy.wait(500);
-  cy.get('.brand-logo').should('be.visible');
+  cy.get('.brand-logo')
+  .should('be.visible');
 });
 
 it('should navigate to register page', () => {
-  cy.visit(Cypress.env('registerUrl'));
-  cy.get('.btn').should('be.visible');
+  cy.visit(Cypress
+    .env('registerUrl'));
+  cy.get('.btn')
+  .should('be.visible');
 });
 
 it('should navigate to login page', () => {
-  cy.visit(Cypress.env('loginUrl'));
+  cy.visit(Cypress 
+    .env('loginUrl'));
   cy.wait(500);
-  cy.get('[type="button"]').should('be.visible');
+  cy.get('[type="button"]')
+  .should('be.visible');
 });
--- client/cypress/integration/Tests/Viewports.spec.js
+++ client/cypress/integration/Tests/Viewports.spec.js
@@ -15,19 +15,27 @@ const phoneModels = [
 describe('Login Page Logo On Mobile', () => {
   phoneModels.forEach((model) => {
     it(`Should display Eventy Logo on ${model} screen`, () => {
-      cy.viewport(model);
-      cy.visit(Cypress.env('baseUrl'));
-      cy.get('.brand-logo').should('be.visible');
+      cy. viewport(model);
+      cy.visit(Cypress.
+        env('baseUrl'));
+      cy.get('.brand-logo')
+      .should('be.visible');
     });
   });
 
-  const macVersions = ['macbook-11', 'macbook-13', 'macbook-15', 'macbook-16'];
+  const macVersions = [
+    'macbook-11',
+    'macbook-13',
+    'macbook-15',
+    'macbook-16'  
+  ];
 
   describe('Login Page Logo On Desktop', () => {
     macVersions.forEach((version) => {
       it(`Should display logo on  ${version}`, () => {
         cy.viewport(version);
-        cy.get('.brand-logo').should('be.visible');
+        cy.get('.brand-logo')
+        .should('be.visible');
       });
     });
   });
